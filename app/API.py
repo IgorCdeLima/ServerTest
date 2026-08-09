efc639a6841b30db38dd7f4c110d5421a2677a05
Igor Cechinato de Lima <igor.c.l.oficial@outlook.com>
2026-08-09 01:14:08 -0300

    feat: atualizacao

--- app/API.py
+++ app/API.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def home():
+    print("Diretório atual:", os.getcwd())
+    print("Arquivos:", os.listdir())
     return FileResponse("index.html")
